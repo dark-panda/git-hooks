2f4cc7e41afe35c3ca44c0145ed73b67f2c5352a
J Smith <dark.panda@gmail.com>
2016-09-12 10:48:37 -0300

    Use spaces instead of tabs.

--- pre-commit.d/whitespace.rb
+++ pre-commit.d/whitespace.rb
@@ -4,17 +4,17 @@ require(File.join(File.expand_path(File.dirname(__FILE__)), *%w{ .. lib shared }
 
 `git rev-parse --verify HEAD`
 against = if $? == 0
-	'HEAD'
+  'HEAD'
 else
-	'4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+  '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 end
 
 files = `git diff-index #{COLOR_UI ? '--color' : ''} --check --cached #{against}`
 unless $? == 0
-	puts "#{msg('/!\\', 'white', 'on_red')} #{msg('WHOA THERE', 'red')}, #{msg('/!\\', 'white', 'on_red')}\n\n"
-	puts "#{msg('Whitespace', 'black', 'on_white')} problems in commit! Take a gander at this:\n\n"
-	puts "#{files}\n\n"
-	exit(1)
+  puts "#{msg('/!\\', 'white', 'on_red')} #{msg('WHOA THERE', 'red')}, #{msg('/!\\', 'white', 'on_red')}\n\n"
+  puts "#{msg('Whitespace', 'black', 'on_white')} problems in commit! Take a gander at this:\n\n"
+  puts "#{files}\n\n"
+  exit(1)
 end
 
 exit(0)
