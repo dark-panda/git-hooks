9d4ad8958acc80bd8da53b6e4545786b52cb72aa
J Smith <dark.panda@gmail.com>
2022-02-25 12:09:37 -0400

    Add a `.ruby-version` swapper.

---
+++ post-merge.d/ruby-version.rb
@@ -0,0 +1,1 @@
+../shared-hooks/ruby-version.rb
---
+++ shared-hooks/ruby-version.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require(File.join(File.expand_path(File.dirname(__FILE__)), *%w{ .. lib git_hooks }))
+
+ROOT_DIR=`git rev-parse --show-cdup`.strip
+
+if !File.exists?("#{ROOT_DIR}.ruby-version") && !File.exists?("#{ROOT_DIR}bin/ruby-version")
+  exit(0)
+end
+
+print GitHooks.msg("\nUpdating Ruby version... ", 'cyan')
+ruby_version = `#{ROOT_DIR}bin/ruby-version`
+
+if $? != 0
+  puts "#{GitHooks.msg("WARNING:", 'red')} tried to switch Ruby versions but..."
+  puts ruby_version
+else
+  puts GitHooks.ok
+end
